d773b77a0d419564177c8999df2e7238831df7e9
Daido Shota <daido1976@gmail.com>
2020-10-10 23:55:55 +0900

    Refactor Lpry

--- lib/lpry.rb
+++ lib/lpry.rb
@@ -2,32 +2,38 @@ require 'lpry/version'
 
 module Lpry
   class InvalidArgument < StandardError; end
+  SINGLETON_SEPARATOR = '.'.freeze
+  INSTANCE_SEPARATOR = '#'.freeze
 
-  # @params method_str [String] e.g. 'CSV.open', 'CSV#each', 'csv.each'
+  # @param method_str [String] e.g. 'CSV.open', 'CSV#each'
   # @return [Boolean]
   def self.show_source(method_str)
-    if method_str.include?('.')
-      c, m = method_str.split('.')
+    method_obj = build_method_object(method_str)
+    print_source(method_obj)
+    true
+  end
+
+  # @param method_str [String] e.g. 'CSV.open', 'CSV#each'
+  # @return [Method, UnboundMethod]
+  def self.build_method_object(method_str)
+    if method_str.include?(SINGLETON_SEPARATOR)
+      c, m = method_str.split(SINGLETON_SEPARATOR)
       klass = Object.const_get(c)
-      method_obj = klass.method(m)
-
-      puts "Lpry prints #{method_obj}"
-
-      file_path, line = method_obj.source_location
-      puts File.readlines(file_path)[line - 1..line + 5]
-      true
-    elsif method_str.include?('#')
-      c, m = method_str.split('#')
+      klass.method(m)
+    elsif method_str.include?(INSTANCE_SEPARATOR)
+      c, m = method_str.split(INSTANCE_SEPARATOR)
       klass = Object.const_get(c)
-      method_obj = klass.instance_method(m)
-
-      puts "Lpry prints #{method_obj}"
-
-      file_path, line = method_obj.source_location
-      puts File.readlines(file_path)[line - 1..line + 5]
-      true
+      klass.instance_method(m)
     else
       raise InvalidArgument
     end
+  end
+
+  # @param method_obj [Method, UnboundMethod]
+  # @return [void]
+  def self.print_source(method_obj)
+    puts "Lpry prints #{method_obj}"
+    file_path, line = method_obj.source_location
+    puts File.readlines(file_path)[line - 1..line + 5]
   end
 end
