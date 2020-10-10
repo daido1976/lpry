2fecff605a5fc9c5ec0398c64e7b0d8eb129e2f5
Daido Shota <daido1976@gmail.com>
2020-10-10 18:29:57 +0900

    Implement Lpry.show_source

--- lib/lpry.rb
+++ lib/lpry.rb
@@ -1,11 +1,25 @@
-require "lpry/version"
+require 'lpry/version'
 
 module Lpry
-  def self.greet
-    "Hello world!"
-  end
+  class InvalidArgument < StandardError; end
 
-  def self.greet!
-    "Hello world!!!"
+  # @params method_str [String] e.g. 'CSV.open', 'CSV#each', 'csv.each'
+  # @return [Boolean]
+  def self.show_source(method_str)
+    if method_str.include?('.')
+      c, m = method_str.split('.')
+      klass = Object.const_get(c)
+      method_obj = klass.method(m)
+
+      puts "Lpry prints #{method_obj}"
+
+      file_path, line = method_obj.source_location
+      puts File.readlines(file_path)[line - 1..line + 5]
+      true
+    elsif method_str.include?('#')
+      # TODO
+    else
+      raise InvalidArgument
+    end
   end
 end
--- spec/lpry_spec.rb
+++ spec/lpry_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe Lpry do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Lpry::VERSION).not_to be nil
   end
 
-  it "says hello world" do
-    expect(Lpry.greet).to eq('Hello world!')
+  describe '.show_source' do
+    context 'when passed the singleton method string' do
+      it 'returns true' do
+        require 'csv'
+        expect(Lpry.show_source('CSV.open')).to eq(true)
+      end
+    end
+
+    context 'when passed the invalid string' do
+      it 'raises InvalidArgument' do
+        expect { Lpry.show_source('') }.to raise_error(Lpry::InvalidArgument)
+      end
+    end
   end
 end
